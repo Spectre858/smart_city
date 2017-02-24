668e6a1450ac5d51d1fd8639c35862aeb91a32e5
Andrey Enschin <spectre858@yandex.ru>
2017-02-24 18:15:13 +0700

    Done - some fix

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -4,6 +4,7 @@
 var user;
 var responseMessageCount = 0;
 var historyMessageCount = 0;
+var activeTab = 1;
 
 function mySub() {
     var login = document.getElementById("login").value;
@@ -49,35 +50,8 @@ function handleUser(userJson) {
 }
 
 function ajaxSuccess(userResp) {
-    var container = document.getElementById("tab-2-table");
-
-    var tr = document.createElement("tr");
-
-    var td1 = document.createElement("td");
-    td1.className = "mdl-data-table__cell--non-numeric";
-    var td2 = document.createElement("td");
-    td2.className = "mdl-data-table__cell--non-numeric";
-    var td3 = document.createElement("td");
-
-    var date = new Date();
-    var dateField = document
-        .createTextNode(date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + " "
-            + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-    var fio = document
-        .createTextNode(userResp.firstname + " " + userResp.lastname + " " + userResp.surname);
-    var email = document
-        .createTextNode(userResp.email);
-
-    td1.appendChild(dateField);
-    td2.appendChild(fio);
-    td3.appendChild(email);
-
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    container.appendChild(tr);
-
-    addHistory("Success");
+    addResponse(userResp);
+    addHistory('success', userResp.email);
 
     responseMessageCount++;
     historyMessageCount++;
@@ -85,13 +59,78 @@ function ajaxSuccess(userResp) {
 }
 
 function ajaxError() {
-    addHistory("Fail");
+    addResponse(null);
+    addHistory('fail', 'User does not exist');
 
+    responseMessageCount++;
     historyMessageCount++;
     updateBadges();
 }
 
-function addHistory(type) {
+function addResponse(userResp) {
+    var container = document.getElementById("tab-2-table");
+    var tr = document.createElement("tr");
+
+    var date = new Date();
+
+    if (userResp) {
+        var td1 = document.createElement("td");
+        td1.className = "mdl-data-table__cell--non-numeric";
+        var td2 = document.createElement("td");
+        td2.className = "mdl-data-table__cell--non-numeric";
+        var td3 = document.createElement("td");
+        td3.className = "mdl-data-table__cell--non-numeric";
+        var td4 = document.createElement("td");
+        td4.className = "mdl-data-table__cell--non-numeric";
+        var td5 = document.createElement("td");
+        td5.className = "mdl-data-table__cell--non-numeric";
+
+        var dateField = document
+            .createTextNode(date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + " "
+                + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+        var first = document
+            .createTextNode(userResp.firstname);
+        var last = document
+            .createTextNode(userResp.lastname);
+        var sur = document
+            .createTextNode(userResp.surname);
+        var email = document
+            .createTextNode(userResp.email);
+
+        td1.appendChild(dateField);
+        td2.appendChild(first);
+        td3.appendChild(last);
+        td4.appendChild(sur);
+        td5.appendChild(email);
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        container.appendChild(tr);
+    } else {
+        var td1 = document.createElement("td");
+        td1.className = "mdl-data-table__cell--non-numeric";
+        var td2 = document.createElement("td");
+        td2.className = "mdl-data-table__cell--non-numeric";
+        td2.colSpan = "4";
+
+        var dateField = document
+            .createTextNode(date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + " "
+                + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+        var text = document
+            .createTextNode("Such user does not exist");
+
+        td1.appendChild(dateField);
+        td2.appendChild(text);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        container.appendChild(tr);
+    }
+}
+
+function addHistory(type, res) {
     var container = document.getElementById("tab-3-table");
 
     var tr = document.createElement("tr");
@@ -101,23 +140,36 @@ function addHistory(type) {
     var td2 = document.createElement("td");
     td2.className = "mdl-data-table__cell--non-numeric";
     var td3 = document.createElement("td");
+    td3.className = "mdl-data-table__cell--non-numeric";
+    var td4 = document.createElement("td");
+    td4.className = "mdl-data-table__cell--non-numeric";
+    var td5 = document.createElement("td");
+    td5.className = "mdl-data-table__cell--non-numeric";
 
     var date = new Date();
     var dateField = document
         .createTextNode(date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + " "
             + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-    var us = document
-        .createTextNode(user.login + " " + user.password);
+    var login = document
+        .createTextNode(user.login);
+    var password = document
+        .createTextNode(user.password);
     var status = document
         .createTextNode(type);
+    var result = document
+        .createTextNode(res);
 
     td1.appendChild(dateField);
-    td2.appendChild(us);
-    td3.appendChild(status);
+    td2.appendChild(login);
+    td3.appendChild(password);
+    td4.appendChild(status);
+    td5.appendChild(result);
 
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
     container.appendChild(tr);
 }
 
@@ -125,6 +177,22 @@ function toast(text) {
     var snackbarContainer = document.querySelector('#demo-toast-example');
     var data = {message: text};
     snackbarContainer.MaterialSnackbar.showSnackbar(data);
+}
+
+function clickTab(tab) {
+    switch (tab) {
+        case 'req':
+            activeTab = 1;
+            break;
+        case 'resp':
+            activeTab = 2;
+            clearBadges(tab);
+            break;
+        case 'hist':
+            activeTab = 3;
+            clearBadges(tab);
+            break;
+    }
 }
 
 function clearBadges(tab) {
@@ -139,6 +207,13 @@ function clearBadges(tab) {
 function updateBadges() {
     var resp = document.getElementById("badges_response");
     var hist = document.getElementById("badges_history");
+
+    if (activeTab == 2) {
+        responseMessageCount = 0;
+    }
+    if (activeTab == 3) {
+        historyMessageCount = 0;
+    }
 
     if (responseMessageCount == 0) {
         resp.style.visibility = "hidden";
